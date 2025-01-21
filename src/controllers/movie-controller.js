8b2ed86734ddbe344883079d9fd2b410404763c2
Ketpul <emiliqnnik07@abv.bg>
2025-01-21 22:00:53 +0200

    update code

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import movieService from "../services/movie-service.js"
+
 const movieController = Router();
 
 movieController.get('/create', (req, res) => {
@@ -8,10 +10,9 @@ movieController.get('/create', (req, res) => {
 
 movieController.get('/:movieId/details', (req, res) => {
     const movieId = req.params.movieId;
+    const movie = movieService.findMovie(movieId);
 
-    const movie = findMovie(movieId);
-
-    res.render('details');
+    res.render('details', {movie});
 });
 
 export default movieController;
