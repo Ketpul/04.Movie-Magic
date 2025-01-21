67de6a0ca989bc0fa0d4d6ee787361915c634a77
Ketpul <emiliqnnik07@abv.bg>
2025-01-21 23:39:54 +0200

    add-search

--- src/movies.js
+++ src/movies.js
@@ -6,6 +6,7 @@ export default [
         description: 'Dreaming about saving countless lives and having another adventure, the feisty Englishfeminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change theworld. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant,wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine AmazonRiver in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper intothe heart of an impenetrable green maze, searching for something that cannot be found, acenturies-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to theirambitious plans.',
         imageUrl: "/static/img/jungle-cruise.jpeg",
         rating: 6.6,
+        year: 2023
     },
     {
         id: 2,
@@ -14,5 +15,6 @@ export default [
         description: 'Blacksmith Will Turner teams up with eccentric pirate "/"Captain"/" Jack Sparrow to save Elizabeth Swann, the governors daughter and his love, from Jacks former pirate allies, who are now undead.',
         imageUrl: "https://m.media-amazon.com/images/M/MV5BMjE5MjkwODI3Nl5BMl5BanBnXkFtZTcwNjcwMDk4NA@@._V1_.jpg",
         rating: 6.6,
+        year: 2021
     }
 ];
