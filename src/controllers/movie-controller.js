6de7da8dc18dc250ca1cf950220aea56d0c19869
Ketpul <emiliqnnik07@abv.bg>
2025-01-21 21:38:24 +0200

    add movie service

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -6,4 +6,12 @@ movieController.get('/create', (req, res) => {
     res.render('create');
 });
 
+movieController.get('/:movieId/details', (req, res) => {
+    const movieId = req.params.movieId;
+
+    const movie = findMovie(movieId);
+
+    res.render('details');
+});
+
 export default movieController;
