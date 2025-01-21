57069db78925cbd0a44bb0b3a58fb926c8ffa6e1
Ketpul <emiliqnnik07@abv.bg>
2025-01-21 20:50:43 +0200

    add dinamic movies on home pages

---
+++ src/movies.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        title: 'Jungle Cuise',
+        genre: 'Adventure',
+        description: 'Dreaming about saving countless lives and having another adventure, the feisty Englishfeminist and doctor of botany, Dr Lily Houghton, embarks on a peril-laden mission to change theworld. Along with her fashionable brother, MacGregor, Dr Houghton enlists the help of the arrogant,wisecracking riverboat skipper, Captain Frank Wolff, to guide them through the serpentine AmazonRiver in La Quila, his swift wooden boat. Now, as the intrepid trio ventures deeper and deeper intothe heart of an impenetrable green maze, searching for something that cannot be found, acenturies-old curse and the ruthless aristocrat, Prince Joachim, threaten to put an end to theirambitious plans.',
+        imageUrl: "/static/img/jungle-cruise.jpeg",
+    },
+    {
+        title: 'Pirates of the Caribbean: The Curse of the Black Pearl',
+        genre: 'Adventure',
+        description: 'Blacksmith Will Turner teams up with eccentric pirate "/"Captain"/" Jack Sparrow to save Elizabeth Swann, the governors daughter and his love, from Jacks former pirate allies, who are now undead.',
+        imageUrl: "https://m.media-amazon.com/images/M/MV5BMjE5MjkwODI3Nl5BMl5BanBnXkFtZTcwNjcwMDk4NA@@._V1_.jpg"
+    }
+];
