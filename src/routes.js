1aa6fb5d1f47e5eaf577a7fc3408e709b572876a
Ketpul <emiliqnnik07@abv.bg>
2025-02-04 17:33:37 +0200

    update

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { Router } from 'express';
 import hoemeController from './controllers/homeController.js';
 import movieController from './controllers/movie-controller.js';
 import castController from './controllers/cast-controller.js';
+import authController from './controllers/auth-controller.js';
 
 const routes = Router();
 
 routes.use(hoemeController);
 routes.use('/movies', movieController);
 routes.use('/casts', castController);
+routes.use('/auth', authController);
 
 routes.get('*', (req, res) => {
     res.render('404');
