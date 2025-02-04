e30f64f736a7b5c27172a5237e6ab0c2055c9871
Ketpul <emiliqnnik07@abv.bg>
2025-02-04 19:07:49 +0200

    update all

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,8 +1,31 @@
-import User from "../models/User.js"
+import User from "../models/User.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const SECRET = "$bpxXLF//NyVoYkfYJgIkdfghjk90zesbqgrvYo7p6qSwiBsRSH/7KRO"
 
 export default {
     register(userData){
         return User.create(userData);
+    },
+    async login(email, password){
+        const user = await User.findOne({ email});
+        if(!user) {
+            throw new Error('Invalid user or password');
+        }
+
+        const isValid = await bcrypt.compare(password, user.password);
+        if(!isValid){
+             throw new Error('Invalid user or password!');
+        }
+
+        const payload = {
+            id: user._id,
+            email: user.email,
+        }
+        const token = jwt.sign(payload, SECRET, {expiresIn: '2h' });
+
+        return token
     }
 
 };
