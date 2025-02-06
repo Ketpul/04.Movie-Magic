7840a1ba0a77a920260a88d176a770a5ac3db805
Ketpul <emiliqnnik07@abv.bg>
2025-02-06 21:10:40 +0200

    add validation movie

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,14 +1,47 @@
 import { Schema, model, Types } from 'mongoose';
 
 const movieSchema = new Schema({
-    title: String, 
+    title: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength:250,
+        match: [/^[a-zA-Z 0-9]+$/,'Director should be alphanumeric, digit and whitespaces only!']
+    }, 
     category: String,
-    genre: String,
-    director: String,
-    year: Number,
-    imageUrl: String,
-    rating: Number,
-    description: String,
+    genre: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength:250,
+        match: [/^[a-zA-Z 0-9]+$/,'Genre should be alphanumeric, digit and whitespaces only!']
+    }, 
+    director: {
+        type: String,
+        required: true,
+        minLength: 5,
+        maxLength:250,
+        match: [/^[a-zA-Z 0-9]+$/,'Director should be alphanumeric, digit and whitespaces only!']
+    }, 
+    year: {
+        type: Number,
+        min: 1900,
+        max: 2025,
+    },
+    imageUrl: {
+        type: String,
+        match: /^https?:\/\//,
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+    },
+    description: {
+        type: String,
+        minLength: 20,
+        match: /^[a-zA-Z 0-9]+$/,
+    },
     casts: [{
         type: Types.ObjectId,
         ref: 'Cast'
